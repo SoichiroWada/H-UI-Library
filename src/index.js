cccb1150126521b907fa0edb6a9ca0c97ca95b61
Soichiro Wada <soichirowada@ipmulticast.net>
2026-08-23 19:24:26 +0900

    second commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 console.log('index.js file');
+
+function gen() {
+    const timestamp = Date.now();
+    const dateObject = new Date(timestamp);
+
+    const day = dateObject.getDate();              // 1 - 31
+    const month = dateObject.getMonth() + 1;       // 1 - 12 (getMonth() is 0-indexed, so Jan is 0)
+    const year = dateObject.getFullYear();         // 4-digit year (e.g., 2026)
+
+    return `Day: ${day}, Month: ${month}, Year: ${year}`;
+}
+
+console.log(gen());
--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,44 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+
   output: {
     path: path.resolve(__dirname, 'dist/assets'),
-    filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
     publicPath: '/assets/'
   },
+
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080,
+
+    static: {
+      directory: path.resolve(__dirname, 'dist')
+    },
+
+    devMiddleware: {
+      publicPath: '/assets/'
+    }
+  },
+
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env']
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+
+        use: {
+          loader: 'babel-loader',
+
+          options: {
+            presets: ['@babel/preset-env']
+          }
         }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
       }
-    }]
+    ]
   }
 };
