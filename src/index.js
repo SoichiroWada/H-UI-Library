b198c80c9a94716e76503a3a5648a1fa026ca148
Soichiro Wada <soichirowada@ipmulticast.net>
2026-08-25 01:10:54 +0900

    6th commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import Tooltip from './ninja-ui/tooltip';
 
-// create tooltip
-const tooltip = new Tooltip(document.querySelector('.tooltip'));
-tooltip.init();
+// create tooltips
+const tooltips = document.querySelectorAll('.tooltip');
+console.log(tooltips);
+console.log(document.querySelectorAll('.tooltip')[0])
+
+//Find the number of tooltips
+const numberOfToolTips = document.querySelectorAll('.tooltip').length
+console.log(numberOfToolTips)
+
+const tooltip1 = new Tooltip(document.querySelectorAll('.tooltip')[0]);
+tooltip1.init();
+
+const tooltip2 = new Tooltip(document.querySelectorAll('.tooltip')[1]);
+tooltip2.init();
+
+const tooltip3 = new Tooltip(document.querySelectorAll('.tooltip')[2]);
+tooltip3.init();
+
