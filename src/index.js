680a4fa22c62e6c6accd94cb3d4188aea04631f1
Soichiro Wada <soichirowada@ipmulticast.net>
2026-08-26 19:02:27 +0900

    10th commit

--- src/index.js
+++ src/index.js
@@ -6,9 +6,8 @@ const tooltips = document.querySelectorAll('.tooltip');
 console.log(tooltips);
 
 tooltips.forEach((tip) => {
-    console.log(tip)
-    const tooltip = new Tooltip(tip)
-    tooltip.init();
+  const tooltip = new Tooltip(tip)
+  tooltip.init();
 });
 
 // create dropdowns
@@ -18,13 +17,3 @@ dropdowns.forEach(dropdown => {
   const instance = new Dropdown(dropdown);
   instance.init();
 });
-
-// //Find the number of tooltips
-// const numberOfToolTips = document.querySelectorAll('.tooltip').length
-// console.log(numberOfToolTips)
-
-// for (let i = 0; i<numberOfToolTips; i++){
-//     const tooltip = new Tooltip(document.querySelectorAll('.tooltip')[i]);
-//     tooltip.init()
-// }
-
