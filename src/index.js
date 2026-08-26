28bf52435978992eae437f77df4b6501ab8784d9
Soichiro Wada <soichirowada@ipmulticast.net>
2026-08-26 18:04:58 +0900

    9th commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Tooltip from './ninja-ui/tooltip';
+import Dropdown from './ninja-ui/dropdown';
 
 // create tooltips
 const tooltips = document.querySelectorAll('.tooltip');
@@ -8,7 +9,15 @@ tooltips.forEach((tip) => {
     console.log(tip)
     const tooltip = new Tooltip(tip)
     tooltip.init();
-})
+});
+
+// create dropdowns
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+  const instance = new Dropdown(dropdown);
+  instance.init();
+});
 
 // //Find the number of tooltips
 // const numberOfToolTips = document.querySelectorAll('.tooltip').length
